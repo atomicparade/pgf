5a7f592b9278748bb6c7428fc55a3872e09d816c
atomicparade <47916875+atomicparade@users.noreply.github.com>
2023-02-26 11:08:13 -0500

    Remove python-gnupg dependency, allow >1 recipient

--- pgf.py
+++ pgf.py
@@ -2,82 +2,111 @@
 
 """Postfix content filter for encrypting outgoing messages with GPG."""
 
+import copy
 import email.policy
 import logging
+import subprocess
 import sys
 from email.message import EmailMessage
 from email.parser import Parser
 from smtplib import SMTP
 from typing import Optional
 
-import gnupg  # type: ignore
+HOST: str = "localhost"
+PORT: int = 10026
+USE_STARTTLS: bool = True
+LOG_FILE: str = "/var/log/pgf.log"
+GPG_HOMEDIR: str = "/var/lib/pgf"
+RECIPIENT_OVERRIDE: str = ""
 
-HOST = "localhost"
-PORT = 10026
-GPG_HOME = "/var/lib/pgf"
-USE_STARTTLS = True
-RECIPIENT_OVERRIDE = ""
-LOG_FILE = "/var/log/pgf.log"
-
-logging.basicConfig(filename=LOG_FILE, encoding="utf-8", level=logging.DEBUG)
+logging.basicConfig(filename=LOG_FILE, encoding="utf-8", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# TODO: Only log for self, not for gnupg
-# TODO: Add relevant logging items
+# TODO: Set logger format
 # TODO: Read config from /etc/pgf.conf
 
 
-gpg = gnupg.GPG(gnupghome=GPG_HOME)
-
-
-def get_gpg_fingerprint(recipient: str) -> Optional[str]:
-    """Search for the recipient's fingerprint. Returns the fingerprint or None."""
-    for key in gpg.list_keys():
-        for uid in key["uids"]:
-            if recipient in uid:
-                return str(key["fingerprint"])
-    return None
-
-
-def encrypt_with_gpg(message: str, fingerprint: str) -> str:
+def attempt_gpg_encryption(message: str, recipient: str) -> Optional[str]:
     """Encrypt a message and return the encrypted message."""
-    return str(gpg.encrypt(message, fingerprint, always_trust=True))
+    encrypted_message = None
+
+    try:
+        with subprocess.Popen(
+            [
+                "gpg",
+                "-ea",
+                "--no-auto-key-locate",  # Do not search key servers
+                "--always-trust",  # Trust already-installed keys
+                "--homedir",
+                GPG_HOMEDIR,
+                "-r",
+                recipient,
+            ],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        ) as gpg_process:
+            output_text, error_text = gpg_process.communicate(message)
+            if "No public key" not in error_text:
+                encrypted_message = output_text
+    except OSError as err:
+        logger.warning("Unable to execute gpg: %s", err)
+
+    return encrypted_message
+
+
+def send_mail(mail: EmailMessage, payload: str, recipient: str) -> None:
+    """Send an mail with the body encrypted, if possible."""
+    recipient_mail = copy.deepcopy(mail)
+
+    recipient_mail.replace_header("To", recipient)
+
+    encrypted_payload = attempt_gpg_encryption(payload, recipient)
+
+    if encrypted_payload:
+        recipient_mail.set_content(encrypted_payload)
+        logger.info("Sending GPG-encrypted message to (%s).", recipient)
+    else:
+        recipient_mail.set_content(payload)
+        logger.info("Sending plaintext message to (%s).", recipient)
+
+    with SMTP(HOST, PORT) as smtp:
+        if USE_STARTTLS:
+            smtp.starttls()
+        smtp.send_message(recipient_mail)
+        smtp.quit()
 
 
 def main() -> None:
     """Called when the program is started."""
     if len(sys.argv) < 2:
-        raise RuntimeError("Missing email recipient.")
+        logger.warning("Missing email recipients.")
+        sys.exit(1)
 
-    recipient = sys.argv[1]
     content = sys.stdin.read()
 
     message = Parser(policy=email.policy.SMTP).parsestr(content)
 
-    msg = EmailMessage()
+    mail = EmailMessage()
 
     for header, value in message.items():
-        msg[header] = value
+        mail[header] = value
 
     payload = message.get_payload()
 
-    if RECIPIENT_OVERRIDE and RECIPIENT_OVERRIDE != "":
-        payload = f"Original message recipient: {msg.get('To', '(none)')}\n\n{payload}"
-        recipient = RECIPIENT_OVERRIDE
-        msg.replace_header("To", RECIPIENT_OVERRIDE)
+    recipients = sys.argv[1:]
 
-    fingerprint = get_gpg_fingerprint(recipient)
-
-    if fingerprint:
-        payload = encrypt_with_gpg(payload, fingerprint)
-
-    msg.set_content(payload)
-
-    with SMTP(HOST, PORT) as smtp:
-        if USE_STARTTLS:
-            smtp.starttls()
-        smtp.send_message(msg)
-        smtp.quit()
+    if (RECIPIENT_OVERRIDE is not None) and (RECIPIENT_OVERRIDE != ""):
+        logger.info(
+            "Overriding recipients (%s) with (%s).",
+            ", ".join(recipients),
+            RECIPIENT_OVERRIDE,
+        )
+        send_mail(mail, payload, RECIPIENT_OVERRIDE)
+    else:
+        for recipient in recipients:
+            send_mail(mail, payload, recipient)
 
     sys.exit(0)
 
